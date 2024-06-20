83a68c78f366ba84553d938199f6cf55e066a2e2
Viciety <llo.gabalca@gmail.com>
2024-06-20 12:44:00 +0200

    Juga només amb peons.

--- ProgramaLogica/MoveKnower.js
+++ ProgramaLogica/MoveKnower.js
@@ -2,12 +2,12 @@ function MovimentPeoBlanc(i, j, taulell){
     let PossiblesMovimentsPeo = new Array()
     if (i != 0){
         if (j != 0){
-            if (taulell.getFitxaEnPosicio(i-1, j-1) != BUIDA){
+            if (taulell.getFitxaEnPosicio(i-1, j-1) > REI_BLANC){
                 PossiblesMovimentsPeo.push(taulell.moveFitxaEnPosicio(i, j, i-1, j-1));
             }
         }
         if (j != 7){
-            if (taulell.getFitxaEnPosicio(i-1, j+1) != BUIDA){
+            if (taulell.getFitxaEnPosicio(i-1, j+1) > REI_BLANC){
                 PossiblesMovimentsPeo.push(taulell.moveFitxaEnPosicio(i, j, i-1, j+1));
             }
         }
@@ -21,12 +21,12 @@ function MovimentPeoNegre(i, j, taulell){
     let PossiblesMovimentsPeo = new Array()
     if (i != 7){
         if (j != 0){
-            if (taulell.getFitxaEnPosicio(i+1, j-1) != BUIDA){
+            if (taulell.getFitxaEnPosicio(i+1, j-1) < PEO_NEGRE && taulell.getFitxaEnPosicio(i+1, j-1) != BUIDA){
                 PossiblesMovimentsPeo.push(taulell.moveFitxaEnPosicio(i, j, i+1, j-1));
             }
         }
         if (j != 7){
-            if (taulell.getFitxaEnPosicio(i+1, j+1) != BUIDA){
+            if (taulell.getFitxaEnPosicio(i+1, j+1) < PEO_NEGRE && taulell.getFitxaEnPosicio(i+1, j+1) != BUIDA){
                 PossiblesMovimentsPeo.push(taulell.moveFitxaEnPosicio(i, j, i+1, j+1));
             }
         }
